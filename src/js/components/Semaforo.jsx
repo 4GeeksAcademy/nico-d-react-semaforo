c5c77d5c4e4de16d33773d23b6417510309e7c70
Nicolas <nicolasdelfino585@gmail.com>
2025-02-18 11:48:44 -0300

    trabajo terminado

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
 const Semaforo = () => {
-  const [colorRojo, setColorRojo] = useState("");
-  const [colorAmarillo, setColorAmarillo] = useState("");
-  const [colorVerde, setColorVerde] = useState("");
+  const [colorRojo, setColorRojo] = useState(false);
+  const [colorAmarillo, setColorAmarillo] = useState(false);
+  const [colorVerde, setColorVerde] = useState(false);
 
-  const handler = () => {
-    if (colorRojo) {
-      setColorRojo;
-    } else if (colorAmarillo) {
-      setColorAmarillo;
-    } else {
-      setColorVerde;
+  const handler = (e) => {
+    if (e.target.classList.contains("rojo")) {
+      setColorRojo(true);
+      setColorAmarillo(false);
+      setColorVerde(false);
+    } else if (e.target.classList.contains("amarillo")) {
+      setColorAmarillo(true);
+      setColorVerde(false);
+      setColorRojo(false);
+    } else if (e.target.classList.contains("verde")) {
+      setColorAmarillo(false);
+      setColorVerde(true);
+      setColorRojo(false);
     }
   };
 
@@ -19,9 +25,18 @@ const Semaforo = () => {
     <div className="container">
       <div className="linea"></div>
       <div className="semaforo">
-        <div className="rojo" onCLick={handler}></div>
-        <div className="amarillo" onCLick={handler}></div>
-        <div className="verde" onCLick={handler}></div>
+        <div
+          className={`rojo ${colorRojo ? "sombra" : ""}`}
+          onClick={handler}
+        ></div>
+        <div
+          className={`amarillo ${colorAmarillo ? "sombra" : ""}`}
+          onClick={handler}
+        ></div>
+        <div
+          className={`verde ${colorVerde ? "sombra" : ""}`}
+          onClick={handler}
+        ></div>
       </div>
     </div>
   );
